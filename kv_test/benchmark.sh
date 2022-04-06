1df44c01785459c1e9076537f0ef0665fb027364
Wenjie Liu <lwjieliu@gmail.com>
2022-04-06 04:24:59 +0000

    Add build and benchmark bash script

---
+++ kv_test/benchmark.sh
@@ -0,0 +1,29 @@
+set -e
+
+DATASET_NAMES=(
+  "books_800M_uint64"
+  "fb_200M_uint64"
+  "osm_cellids_800M_uint64"
+  "wiki_ts_200M_uint64"
+)
+
+ROOT=$1
+echo "${ROOT}"
+
+mkdir -p ${ROOT}/temp/pgm
+mkdir -p ${ROOT}/storage/pgm
+mkdir -p ${ROOT}/out
+
+echo "Start PGM Benchmark"
+
+for ((i = 0; i < ${#DATASET_NAMES[@]}; i++)) do
+    for ((j = 0; j < 10; j++)) do
+        dataset_name="${DATASET_NAMES[$i]}"
+       	echo ">>> ${dataset_name} ${j}"
+	bash ./reload_local.sh
+	./kv_test/kv_benchmark \
+   	    --key_path=${ROOT}/keyset/${dataset_name}_ks \
+            --target_db_path=${ROOT}/storage/pgm/${dataset_name} \
+	    --out_path=${ROOT}/out/${dataset_name}_out.txt
+    done
+done
